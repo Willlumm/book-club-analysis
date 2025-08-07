50b2cec8e05e9bb38944d5705126024e88f57089
William Lumme <80401792+Willlumm@users.noreply.github.com>
2025-08-07 22:06:18 +0100

    Add comment

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 
-os.environ["PYPPETEER_CHROMIUM_REVISION"] = "1498390"
+# Need to set Chromium version before importing requests-html. Default version used by
+# Pyppeteer is no longer available.
+os.environ["PYPPETEER_CHROMIUM_REVISION"] = "1465706"
 
 from dotenv import load_dotenv
 from requests_html import HTMLSession
